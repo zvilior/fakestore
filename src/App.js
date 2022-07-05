a678e497470f5d8388554fe8058aadc57c95a4a0
zvilior <102918963+zvilior@users.noreply.github.com>
2022-07-05 14:03:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Main from './cmponents/Main';
+import Headers from './cmponents/Header'
+import Cart from './cmponents/Cart';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Headers /> */}
+      <Main />
+      <Cart />
     </div>
   );
 }
